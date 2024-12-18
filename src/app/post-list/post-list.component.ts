4d138b395ad801df2e27d1a9f05fac7711c20efd
Mhd.Anggie Rinovka <45094075+MhdAnggieRinovka@users.noreply.github.com>
2024-12-18 16:22:01 +0700

    Progress now in child to parent

---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,26 @@
+import { NgClass, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-post-list',
+  imports: [NgIf, FormsModule,NgSwitch,NgSwitchCase,NgSwitchDefault,NgClass],
+  templateUrl: './post-list.component.html',
+  styleUrl: './post-list.component.css'
+})
+export class PostListComponent {
+  // untuk ambil inputan dari class parentnya pakai @input 
+@Input() postTitle : string ='' ;
+@Input() postLogin :boolean = false;
+childMessage : string = "Ini dari post list (kelas children)"
+postCount : number = 190;
+parentMessage : string ="Message from child using click Event"
+
+@Output () MessageEvent = new EventEmitter();
+
+  sendMessage()
+  {
+    // console.log("test");
+    this.MessageEvent.emit(this.parentMessage)
+  }
+}
